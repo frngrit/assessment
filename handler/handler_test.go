f59d555c04f26366f6e181afe93bc37d50d18e3e
frngrit <frank.12876@hotmail.com>
2023-01-06 18:52:33 +0700

    test: add unit test of GetExpenseById

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -18,7 +18,7 @@ import (
 func TestCreateExpense(t *testing.T) {
 	// Arrange
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/expense", strings.NewReader(`{
+	req := httptest.NewRequest(http.MethodPost, "/expenses", strings.NewReader(`{
 		"title": "strawberry smoothie",
 		"amount": 79,
 		"note": "night market promotion discount 10 bath", 
@@ -58,6 +58,50 @@ func TestCreateExpense(t *testing.T) {
 	// Assertions
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
+	}
+}
+
+func TestGetLatestExpense(t *testing.T) {
+	// Arrange
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/expenses", strings.NewReader(``))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	expenseMock := Expense{
+		1,
+		"strawberry smoothie",
+		79,
+		"night market promotion discount 10 bath",
+		[]string{"food", "beverage"},
+	}
+
+	newsMockRows := sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
+		AddRow(expenseMock.Id, expenseMock.Title, expenseMock.Amount, expenseMock.Note, pq.Array(expenseMock.Tags))
+
+	mock.ExpectPrepare("SELECT")
+	mock.ExpectQuery("SELECT (.+) FROM expenses").WithArgs("1").WillReturnRows(newsMockRows)
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	h := handler{db}
+	c := e.NewContext(req, rec)
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+	expected := "{\"id\":1,\"title\":\"strawberry smoothie\",\"amount\":79,\"note\":\"night market promotion discount 10 bath\",\"tags\":[\"food\",\"beverage\"]}"
+
+	// Act
+	err = h.GetExpenseById(c)
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 	}
 }
